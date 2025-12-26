f75eaf563eaa1ae2f30fb6cbc28cf4884ed10b1e
SyedShakirX <159051009+SyedShakirX@users.noreply.github.com>
2025-12-26 16:01:08 +0530

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -5,4 +5,4 @@ set -e
 docker pull public.ecr.aws/n2p7k0f2/aws-ci-cd-python-flask:latest
 
 #I have enabled port 8000 on Security Group attached to my EC2
-docker run -p 5000:8000 aws-ci-cd-python-flask:latest
+docker run -p 5000:8000 public.ecr.aws/n2p7k0f2/aws-ci-cd-python-flask:latest
